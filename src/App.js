8ce683145005af1ecdc39adc3cb86a86c7899a26
Robert Dawson <manatees@gmail.com>
2020-03-24 21:28:46 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+import Map from './Map';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: '',
+      error: null,
+      isLoaded: false,
+      locations: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      'https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=US&source=csbs',
+    )
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            locations: result.locations,
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
+
+  render() {
+    const { error, isLoaded, locations } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <Map />
+          <ul>
+            {locations.map(location => (
+              <li key={location.id}>
+                <strong>
+                  {location.state} - {location.county}
+                </strong>
+                <br />
+                confirmed: {location.latest.confirmed.toLocaleString('en')}
+                <br />
+                deaths: {location.latest.deaths.toLocaleString('en')}
+                <br />
+                recovered: {location.latest.recovered.toLocaleString('en')}
+                <br />
+                <br />
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/Map.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { GoogleMap, LoadScript, Autocomplete } from '@react-google-maps/api';
+
+const mapContainerStyle = {
+  height: '400px',
+  width: '800px',
+};
+
+const center = {
+  lat: 38.685,
+  lng: -115.234,
+};
+
+const zoom = 2.5;
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: center,
+      zoom: zoom,
+    };
+
+    this.autocomplete = null;
+
+    this.onLoad = this.onLoad.bind(this);
+    this.onPlaceChanged = this.onPlaceChanged.bind(this);
+  }
+
+  onLoad(autocomplete) {
+    console.log('autocomplete: ', autocomplete);
+    this.autocomplete = autocomplete;
+  }
+
+  onPlaceChanged() {
+    if (this.autocomplete !== null) {
+      console.log(this.autocomplete.getPlace());
+      this.setState({
+        center: {
+          lat: this.autocomplete.getPlace().geometry.location.lat(),
+          lng: this.autocomplete.getPlace().geometry.location.lng(),
+        },
+        zoom: 5,
+      });
+    } else {
+      console.log('Autocomplete is not loaded yet!');
+    }
+  }
+
+  render() {
+    return (
+      <LoadScript
+        id="script-loader"
+        googleMapsApiKey="AIzaSyAH-q6sCWB5FSS2iDGbXXdm6EhpnyF_118"
+        libraries={['places']}
+      >
+        <GoogleMap
+          id="searchbox-example"
+          mapContainerStyle={mapContainerStyle}
+          zoom={this.state.zoom}
+          center={this.state.center}
+        >
+          <Autocomplete
+            onLoad={this.onLoad}
+            onPlaceChanged={this.onPlaceChanged}
+          >
+            <input
+              type="text"
+              placeholder="Place"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: 'absolute',
+                left: '50%',
+                marginLeft: '-120px',
+              }}
+            />
+          </Autocomplete>
+        </GoogleMap>
+      </LoadScript>
+    );
+  }
+}
+
+export default Map;
